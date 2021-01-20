17d6d7f7756a0aaa109834df90b6eaa5d717df76
Sky Park <hyunwoo126@gmail.com>
2021-01-19 19:46:28 -0800

    response set header to json

--- functions/slack-interactive-handler.js
+++ functions/slack-interactive-handler.js
@@ -2,7 +2,8 @@ module.exports = ({ functions }) => {
   return async (req, res) => {
     const { payload } = req.body;
     functions.logger.log('--- req.body.payload ---', payload);
-    res.sendStatus(200);
+    res.setHeader('content-type', 'application/json');
+    res.status(200);
   };
 };
 
