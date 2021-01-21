1daf4e29f206bcc94174e3b646e37a38d91ac8ba
Sky Park <hyunwoo126@gmail.com>
2021-01-20 19:47:57 -0800

    add to firestore

--- functions/slack-interactive-handler.js
+++ functions/slack-interactive-handler.js
@@ -2,6 +2,16 @@ module.exports = ({ functions, db }) => {
   return async (req, res) => {
     const { payload } = req.body;
     functions.logger.log('--- req.body.payload ---', payload);
+    const formData = Object.values(payload.values).reduce((accum, current) => {
+      return Object.assign(accum, current);
+    }, {});
+
+    await db.collection('posts').add({
+      price: formData.price.value,
+      title: formData.title.value,
+      description: formData.description.value,
+    });
+
     res.status(200);
     res.json({
       response_action: 'clear',
