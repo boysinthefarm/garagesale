c789564ab595215e74ad60d9745dd82262cc712e
Sky Park <hyunwoo126@gmail.com>
2021-01-27 21:34:08 -0800

    get user posts

--- functions/mylist-handler.js
+++ functions/mylist-handler.js
@@ -1,4 +1,10 @@
-module.exports = (req, res) => {
+const { db } = require('./utils');
+
+module.exports = async (req, res) => {
+  const user = req.body.user_id;
+
+  const userPosts = await db.collection('posts').where('seller', '==', user).get();
+
   res.sendStatus(200);
 };
 
