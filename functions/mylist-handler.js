894c8b08f96bf137bb7214a0eb7810eeda868885
Sky Park <hyunwoo126@gmail.com>
2021-01-27 22:22:06 -0800

    mylist responds with my posts

--- functions/mylist-handler.js
+++ functions/mylist-handler.js
@@ -1,10 +1,22 @@
-const { db } = require('./utils');
+const { db, webClientBot } = require('./utils');
+const getPostBlock = require('./block-kits');
 
 module.exports = async (req, res) => {
-  const user = req.body.user_id;
+  // these two requests should be parallel
+  const userInfo = await webClientBot.users.info({ user: req.body.user_id });
+  const posts = await db.collection('posts').where('seller', '==', user).get();
 
-  const userPosts = await db.collection('posts').where('seller', '==', user).get();
+  const blocks = [];
+  posts.forEach(doc => {
+    blocks = blocks.concat(getPostBlock({
+      ...doc.data(),
+      display_name: userInfo.user.profile.display_name,
+    }));
+  });
 
-  res.sendStatus(200);
+  res.send({
+    response_type: 'in_channel',
+    blocks,
+  });
 };
 
