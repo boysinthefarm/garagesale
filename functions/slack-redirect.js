1acf6d4f0eaaf5125820b18f8ef1ee6f1c692f20
Sky Park <hyunwoo126@gmail.com>
2021-02-11 19:14:33 -0800

    use slack nodejs oauth package

--- functions/slack-redirect.js
+++ functions/slack-redirect.js
@@ -1,6 +1,17 @@
+const functions = require('firebase-functions');
+const { InstallProvider } = require('@slack/oauth');
 const { logger } = require('./utils');
+
+const {
+  client_id: clientId,
+  client_secret: clientSecret,
+} = functions.config().slack;
+
+// initialize the installProvider
+const installer = new InstallProvider({ clientId, clientSecret });
 
 module.exports = (req, res) => {
   logger.log('---- redirect ----', req.body);
+  installer.handleCallback(req, res);
 };
 
