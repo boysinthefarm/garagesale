2af287916da1e5f6fd374b011bf33aa0f5561214
Sky Park <hyunwoo126@gmail.com>
2021-02-15 11:02:25 -0800

    add to slack img

--- functions/index.js
+++ functions/index.js
@@ -7,13 +7,14 @@ const slackEvents = require('./slack-events');
 const slackInteractive = require('./slack-interactive');
 const slackRedirect = require('./slack-redirect');
 
+const imgAddToSlack = '<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />';
 const app = express();
 app.get('/', async(req, res) => {
   const url = await generateInstallUrl();
 
   res.send(`<html><body>
     <h1>slack-garage-sale</h1>
-    <a href="${url}">${url}</a>
+    <a href="${url}">${imgAddToSlack}</a>
   </body></html>`);
 });
 exports.app = functions.https.onRequest(app);
