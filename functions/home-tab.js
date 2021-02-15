9efbe661a190a61c322718ce74ef767e1f642bd2
Paul Jeon <yjeon0@gmail.com>
2021-02-15 12:08:08 -0800

    revert partial functionality

--- functions/home-tab.js
+++ functions/home-tab.js
@@ -16,43 +16,15 @@ async function renderHomeTab(event) {
     }
   } = await client.users.info({ user: userId });
 
-  const allItems = ...await listCommandBlock({ userId, teamId });
-  const myItems = ...await getMylistBlocks({ userId });
-  const mySoldItems = ...await getMyListHistoryBlocks({ userId });
-
-  // build blocks based on returned values form queries
   const blocks = [
     headerBlock('Welcome :partying_face: \n Check out the items in the marketplace! :kite:'),
-    divider,
-  ];
-
-  if (allItems == []) {
-    const showAllItems = 'There are currently no items available for sale. Please comet back later!';
-  } else {
-    const showAllItems = allItems;
-  }
-
-  if (myItems == []) {
-    const showMyItems = 'You have not listed any items! Please refer to Message or About tab for any guidance.';
-  } else {
-    const showMyItems = myItems;
-  }
-
-  if (mySoldItems == []) {
-    const = showMySoldItems = 'Once you mark an item as sold, it will appear here!'
-  } else {
-    const showMySoldItems = mySoldItems
-  }
-
-  const blocks = [
-    headerBlock('Welcome :partying_face: \n Check out the items in the marketplace! :kite:'),
-    showAllItems
+    ...await listCommandBlock({ userId, teamId }),
     divider,
     headerBlock('Your Lemonade Stand :lemon:'),
-    showMyItems,
+    ...await getMylistBlocks({ userId }),
     divider,
     headerBlock('Your Sold Items :moneybag:'),
-    showMySoldItems,
+    ...await getMyListHistoryBlocks({ userId }),
   ];
 
   return client.views.publish({
