f737b1813b80a1ac8c7e7e10e396468595e255ff
Paul Jeon <yjeon0@gmail.com>
2021-02-15 12:19:29 -0800

    empty state for current user's listings

--- functions/mylist-handler.js
+++ functions/mylist-handler.js
@@ -1,4 +1,4 @@
-const { getPostBlock, myPostActionButtons } = require('./block-kits');
+const { getPostBlock, myPostActionButtons, getMrkdwnBlock } = require('./block-kits');
 const { db, PostsApi } = require('./db-api');
 const { botClientFactory } = require('./slack-installer');
 
@@ -30,6 +30,10 @@ const getMylistBlocks = async ({ userId }) => {
     }, buttons ? [buttons] : undefined));
   });
 
+  // handle empty state
+  if (blocks == []) {
+    blocks.concat(getMrkdwnBlock('You have not listed any items! Please refer to Message or About tab for any guidance.'));
+  }
   return blocks;
 };
 
