afebc82f2a771cc65f6d161c9c333fea32439903
Sky Park <hyunwoo126@gmail.com>
2021-02-15 10:43:06 -0800

    cleanup

--- functions/utils.js
+++ functions/utils.js
@@ -1,13 +1,7 @@
 const functions = require('firebase-functions');
-const { WebClient } = require('@slack/web-api');
 const { db } = require('./db-api');
 
 const APP_NAME = 'Lemonade';
-
-const {
-  token,
-  user_token: userToken,
-} = functions.config().slack;
 
 module.exports = {
   APP_NAME,
