54c5cb68df0d8c969828cf9f4901639513eb64dc
Sky Park <hyunwoo126@gmail.com>
2021-02-20 12:41:28 -0800

    oh fix functions import

--- functions/pub-sub/handlers.js
+++ functions/pub-sub/handlers.js
@@ -1,6 +1,8 @@
-const { pubsub } = require('firebase-functions');
+const functions = require('firebase-functions');
 const TOPIC = require('./topic');
 const { logger } = require('./../utils');
+
+const pubsub = functions.pubsub;
 
 exports.helloPubSub = pubsub.topic(TOPIC.hello).onPublish((message) => {
   logger.log('---- helloPubSub ----', message.json);
