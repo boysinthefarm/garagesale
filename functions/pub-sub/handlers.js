b8c690e455e9bd57c32daad8a3aed9297ee6ea37
Sky Park <hyunwoo126@gmail.com>
2021-02-20 12:38:27 -0800

    fix require parent dir

--- functions/pub-sub/handlers.js
+++ functions/pub-sub/handlers.js
@@ -1,6 +1,6 @@
 const { pubsub } = require('firebase-functions');
 const TOPIC = require('./topic');
-const { logger } = require('../utils');
+const { logger } = require('./../utils');
 
 exports.helloPubSub = pubsub.topic(TOPIC.hello).onPublish((message) => {
   logger.log('---- helloPubSub ----', message.json);
