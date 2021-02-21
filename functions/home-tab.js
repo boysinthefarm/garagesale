05c7d7f7f27294c8c2990e09d8e7acc108c7facd
Sky Park <hyunwoo126@gmail.com>
2021-02-20 16:30:45 -0800

    need to break out array

--- functions/home-tab.js
+++ functions/home-tab.js
@@ -5,6 +5,7 @@ const {
 } = require('./block-kits');
 const { getMylistBlocks, getMyListHistoryBlocks } = require('./mylist-handler');
 const { botClientFactory } = require('./slack-installer');
+const { logger } = require('./utils');
 
 async function renderHomeTab(auth) {
   const client = await botClientFactory(auth);
@@ -13,14 +14,16 @@ async function renderHomeTab(auth) {
   const blocks = await Promise.all([
     headerBlock('Welcome :partying_face: \n Check out the items in the marketplace! :kite:'),
     divider,
-    listCommandBlock({ userId, teamId }),
+    listCommandBlock({ userId, teamId }).then(block => ...block),
     headerBlock('Your Lemonade Stand :lemon:'),
     divider,
-    getMylistBlocks({ userId, teamId }),
+    getMylistBlocks({ userId, teamId }).then(block => ...block),
     headerBlock('Your Sold Items :moneybag:'),
     divider,
-    getMyListHistoryBlocks({ userId, teamId }),
+    getMyListHistoryBlocks({ userId, teamId }).then(block => ...block),
   ]);
+
+  logger.log('renderHomeTab blocks', blocks);
 
   return client.views.publish({
     user_id: userId,
