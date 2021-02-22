952d2074c80520238d956be43f8a8033b24845ab
Sky Park <hyunwoo126@gmail.com>
2021-02-21 16:06:33 -0800

    basic setup for web

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const { PubSub } = require('@google-cloud/pubsub');
+const engines = require('consolidate');
 const { generateInstallUrl } = require('./slack-installer');
 const { APP_NAME } = require('./utils');
 
@@ -11,16 +12,20 @@ const slackEvents = require('./slack-events');
 const slackInteractive = require('./slack-interactive');
 const slackRedirect = require('./slack-redirect');
 
-const imgAddToSlack = '<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />';
+const installUrl = generateInstallUrl();
+  
 const web = express();
-web.get('/', async(req, res) => {
-  const url = await generateInstallUrl();
+web.engine('hbs', engines.handlebars);
+web.set('views', './views');
+web.set('view engine', 'hbs');
 
-  res.send(`<html><body>
-    <h1>${APP_NAME}</h1>
-    <a href="${url}">${imgAddToSlack}</a>
-  </body></html>`);
+web.get('/', async(req, res) => {
+  res.render('index', {
+    installUrl: await installUrl,
+    APP_NAME,
+  });
 });
+
 exports.web = functions.https.onRequest(web);
 
 exports.slackBot = functions.https.onRequest(slackEvents);
